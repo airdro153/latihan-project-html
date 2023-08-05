bb2db57d3961ae0b72c49cefb77d951cb4d878ad
haikal059 <43695662+haikal059@users.noreply.github.com>
2023-08-05 16:11:11 +0800

    make file script.js and modife index.html

---
+++ latihan1/script.js
@@ -0,0 +1,15 @@
+function FollowTw() {
+    window.open('https://twitter.com/Haikalyusrizall')
+}
+
+function FollowInstagram() {
+    window.open('https://www.instagram.com/haikal059/')
+}
+
+function Chattelegram() {
+    window.open('https://telegram.me/yorusa12')
+}
+
+function ChatWhatsapp() {
+    window.open('https://wa.me/6281342246076')
+}
